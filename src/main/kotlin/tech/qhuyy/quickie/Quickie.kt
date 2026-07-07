e188fbfae46fdb68d399736f4a0fe4b77826b06d
hqng05 <hqng05@qhuyy.tech>
2026-07-07 16:33:48 +0700

    refactor(core): wire database, economy and message managers into plugin lifecycle
    Register DatabaseManager, EconomyManager + VaultEconomyProvider (ServicesManager,
    Normal priority) and MessageManager in onEnable; unload MessageManager and shut down
    the pool in onDisable. All managers receive their dependencies via constructor.

--- src/main/kotlin/tech/qhuyy/quickie/Quickie.kt
+++ src/main/kotlin/tech/qhuyy/quickie/Quickie.kt
@@ -2,12 +2,20 @@ package tech.qhuyy.quickie
 
 import com.tcoded.folialib.FoliaLib
 import net.kyori.adventure.platform.bukkit.BukkitAudiences
+import net.milkbowl.vault.economy.Economy
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.plugin.ServicePriority
 import tech.qhuyy.quickie.config.ConfigManager
+import tech.qhuyy.quickie.database.DatabaseManager
+import tech.qhuyy.quickie.manager.MessageManager
+import tech.qhuyy.quickie.economy.EconomyListener
+import tech.qhuyy.quickie.economy.EconomyManager
+import tech.qhuyy.quickie.economy.VaultEconomyProvider
 import tech.qhuyy.quickie.manager.MetricsManager
 import tech.qhuyy.quickie.scheduler.PluginCoroutineScope
 import tech.qhuyy.quickie.utils.PluginBuild
 import tech.qhuyy.quickie.utils.Software
+import kotlinx.coroutines.runBlocking
 
 private const val PLUGIN_ID = 32424
 
@@ -24,7 +32,13 @@ class Quickie : JavaPlugin() {
         private set
     lateinit var configManager: ConfigManager
         private set
+    lateinit var messageManager: MessageManager
+        private set
     lateinit var metricsManager: MetricsManager
+        private set
+    lateinit var databaseManager: DatabaseManager
+        private set
+    lateinit var economyManager: EconomyManager
         private set
 
     override fun onEnable() {
@@ -37,7 +51,29 @@ class Quickie : JavaPlugin() {
         bukkitAudiences = BukkitAudiences.create(this)
         scope = PluginCoroutineScope(this)
         configManager = ConfigManager(this).also { it.init() }
+        messageManager = MessageManager(this, bukkitAudiences).also { it.load() }
         metricsManager = MetricsManager(this, PLUGIN_ID).also { it.start() }
+
+        databaseManager = DatabaseManager(this, configManager, scope)
+        try {
+            databaseManager.connect()
+        } catch (e: Exception) {
+            logger.severe("Economy/database disabled: ${e.message}")
+        }
+
+        if (databaseManager.isConnected) {
+            economyManager = EconomyManager(this, databaseManager, configManager)
+            runBlocking { economyManager.init() }
+
+            val provider = VaultEconomyProvider(this, economyManager, configManager, databaseManager)
+            server.servicesManager.register(
+                Economy::class.java,
+                provider,
+                this,
+                ServicePriority.Normal
+            )
+            server.pluginManager.registerEvents(EconomyListener(this, economyManager), this)
+        }
     }
 
     private fun logScheduleStatus() {
@@ -66,7 +102,9 @@ class Quickie : JavaPlugin() {
     }
 
     override fun onDisable() {
+        messageManager.unload()
+        server.servicesManager.unregisterAll(this)
+        databaseManager.shutdown()
         scope.cancel()
-        // Plugin shutdown logic
     }
 }
